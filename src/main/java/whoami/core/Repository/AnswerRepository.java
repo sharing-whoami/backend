59b931ff7ba4bb172d6db3c7ae8fd6b32196596a
yeonsssu26 <monica0326@khu.ac.kr>
2022-03-26 20:45:08 +0900

    ADD : Repository (Answer)

--- src/main/java/whoami/core/Repository/AnswerRepository.java
+++ src/main/java/whoami/core/Repository/AnswerRepository.java
@@ -1,2 +1,15 @@
-package whoami.core.Repository;public class AnswerRepository {
+package whoami.core.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import whoami.core.Domain.Answer;
+
+import java.util.List;
+
+
+public interface AnswerRepository extends JpaRepository<Answer, Long>{
+
+//    @Query("SELECT a FROM Answers a ORDER BY a.id DESC")
+    List<Answer> findAll();
+
 }
